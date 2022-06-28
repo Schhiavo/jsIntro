7689ec85062340ca7567ffd069631786538d1aba
Victor Schiavo <victorschiavo343@gmail.com>
2022-06-28 20:08:47 -0300

    adiciona const e let

---
+++ exm04.js
@@ -0,0 +1,6 @@
+let a = 10; // funciona para declarar uma variavél, porém, ele não "iça" as variaveis 
+const b = 20; // funciona para declarar uma variável, porém, é um valor absoluto, ou seja, não pode mudar
+const c = a+b;
+console.log(a, b, c); //  printa o conteúdo num console
+a = 15;
+b = 40; // tem um erro aqui mas não mudei pra deixar como exemplo
