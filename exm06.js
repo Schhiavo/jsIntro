e5fb96771dbca77d62c45c518fa3352dd8eb65da
Victor Schiavo <victorschiavo343@gmail.com>
2022-06-28 20:33:50 -0300

    adiciona vetor

---
+++ exm06.js
@@ -0,0 +1,11 @@
+const vetor = [5, 4, 3, 2, 1]; // cria vetor
+vetor [0] = 1; // manipula vetor
+vetor [2] = 1;
+console.log(vetor); // imprime vetor
+vetor.push(7); // push = empilhar, ele adiciona um numero ao vetor
+vetor.push(8);
+console.log(vetor);
+vetor.pop(); // pop = desempilhar, ele remove o numero
+console.log(vetor.pop()); // remove o numero e imprime
+console.log(vetor);
+console.log(vetor.length); // conta e retorna o numero de elementos que o vetor tem
