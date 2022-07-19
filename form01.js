cf12f62ea072c9f293cb0fc2c41e5a194477b557
victor schiavo <victorschiavo343@gmail.com>
2022-07-19 20:32:35 -0300

    Acrescenta formulario com reset no html/js

---
+++ form01.js
@@ -0,0 +1,16 @@
+function criaElemento(cor, altura, largura){
+    const eNovo = document.createElement("div");
+    eNovo.textContent = `${cor} ${altura} ${largura}`;
+    document.body.appendChild(eNovo);
+}
+document.exm01.addEventListener('submit', submitListener);
+
+function submitListener(event){
+    event.preventDefault();
+criaElemento(
+    document.exm01.cor.value,
+    document.exm01.altura.value,
+    document.exm01.largura.value
+);
+document.exm01.reset();
+}
