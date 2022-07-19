f4327c737c3ff109db059d7d900614c4795d73eb
victor schiavo <victorschiavo343@gmail.com>
2022-07-19 20:56:32 -0300

    Finalizado

--- form01.js
+++ form01.js
@@ -1,16 +1,24 @@
+let estado = {
+    cor:"#000000",
+    largura: 0,
+    altura: 0,
+};
+
 function criaElemento(cor, altura, largura){
     const eNovo = document.createElement("div");
     eNovo.textContent = `${cor} ${altura} ${largura} ${altura+largura}`;
     document.body.appendChild(eNovo);
+    eNovo.style=`color: ${cor};`
 }
 document.exm01.addEventListener('submit', submitListener);
 
 function submitListener(event){
     event.preventDefault();
-criaElemento(
-    document.exm01.cor.value,
-    Number(document.exm01.altura.value), //Number está convertendo o texto em numero
-    Number(document.exm01.largura.value)
-);
+    estado.cor = document.exm01.cor.value;
+    estado.altura = Number(document.exm01.altura.value);
+    estado.largura = Number(document.exm01.largura.value);
+criaElemento(estado.cor, estado.altura, estado.largura);
+   
+
 document.exm01.reset();
 }
